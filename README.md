585189aa14e349d3babef5d7a4ee66e7c98e70f7
ANGELSANDOVALMENDOZA <angelsandoval@ciencias.unam.mx>
2024-08-27 00:05:03 -0600

    implementación de filtro mosaico

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Editor-de-Fotos-Python
 implementación de algunos de los filtros vistos en la clase de Proceso Digital de Imagenes 
+
+## Requisitos 
+
+- Python 3.x
+- tkinter
+- Pillow
+
+## Uso
+
+1. Ejecuta el script utilizando el comando "editor.py".
+2. Cargar la imagen con el boton "Cargar Imagen"
+3. Escoger el filtro a utilizar de la lista de selección
+
+## Notas
+
+* En esta versión el valor a aumentar el brillo es constante, si se desea cambiar solo modificar el valor de la constante aumento de la función brillo
+* Para poder ver los demas filtros utilizar las flechas del teclado para poder moverse por la lista
--- editor.py
+++ editor.py
@@ -52,6 +52,14 @@ def aplicar_filtro(event):
             tonos_gris2()
         elif filtro_seleccionado == "Gris3":
             tonos_gris3()
+        elif filtro_seleccionado == "Brillo":
+            brillo()
+        elif filtro_seleccionado == "Alto Contraste":
+            alto_contraste()
+        elif filtro_seleccionado == "Inverso":
+            inverso()
+        elif filtro_seleccionado == "Mosaico":
+            mosaico()
 
 # Filtros de color
 def tonos_gris1():
@@ -102,6 +110,77 @@ def tonos_azules():
         imagen_azul.putdata(azul)
         mostrar_imagen(imagen_azul)
 
+def brillo():
+    global imagen
+    aumento = 0.5
+    if imagen:
+        imagen_brillo = imagen.convert("RGB")
+        brillo = [tuple(min(int(aumento * c), 255) for c in (r, g, b)) for (r, g, b) in imagen_brillo.getdata()]
+        imagen_brillo.putdata(brillo)
+        mostrar_imagen(imagen_brillo)
+
+def alto_contraste():
+    global imagen
+    tonos_gris2()
+    if imagen:
+        alta_imagen = imagen.convert("RGB")
+        contraste = []
+        for (r, g, b) in alta_imagen.getdata():
+            c = ((r+g+b)//3) 
+            if c < 127:
+                contraste.append((0, 0, 0))   
+            else:
+                contraste.append((255, 255, 255))
+        alta_imagen.putdata(contraste)
+        mostrar_imagen(alta_imagen)
+
+def inverso():
+    global imagen
+    tonos_gris2()
+    if imagen:
+        alta_imagen = imagen.convert("RGB")
+        contraste = []
+        for (r, g, b) in alta_imagen.getdata():
+            c = ((r+g+b)//3) 
+            if c < 127:
+                contraste.append((255, 255, 255))   
+            else:
+                contraste.append((0, 0, 0))
+        alta_imagen.putdata(contraste)
+        mostrar_imagen(alta_imagen)
+
+def mosaico():
+    global imagen
+    tamano_bloque = 10
+    if imagen:
+        imagen_mosaico = imagen.convert("RGB")
+        ancho, alto = imagen_mosaico.size
+        pixeles = imagen_mosaico.load()
+
+        for y in range(0, alto, tamano_bloque):
+            for x in range(0, ancho, tamano_bloque):
+                # Definir el área del bloque
+                area = (x, y, min(x + tamano_bloque, ancho), min(y + tamano_bloque, alto))
+                bloque = imagen_mosaico.crop(area)
+                
+                # Calcular el color promedio del bloque
+                promedio_color = bloque.resize((1, 1)).getpixel((0, 0))
+                
+                # Aplicar el color promedio a todo el bloque
+                for i in range(tamano_bloque):
+                    for j in range(tamano_bloque):
+                        if x + i < ancho and y + j < alto:
+                            pixeles[x + i, y + j] = promedio_color
+
+        mostrar_imagen(imagen_mosaico)
+
+
+
+
+
+                        
+        
+
 # Botones y lista
 boton_cargar = tk.Button(ventana, text="Cargar Imagen", command=cargar_imagen)
 boton_cargar.pack(pady=20)
@@ -109,7 +188,7 @@ boton_cargar.pack(pady=20)
 cuadro_lista = tk.Listbox(ventana, height=4, width=15, selectmode="single")
 cuadro_lista.pack()
 
-filtros = ["Rojo", "Azul", "Verde", "Gris1","Gris2","Gris3"]
+filtros = ["Rojo", "Azul", "Verde", "Gris1","Gris2","Gris3","Brillo","Alto Contraste","Inverso","Mosaico"]
 
 for filtro in filtros:
     cuadro_lista.insert(tk.END, filtro)
